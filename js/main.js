2e6cdf68db128b6f055d7373dd5892d22e5a748b
kaliraj <kalisdvk96@gmail.com>
2023-02-01 22:14:25 +0530

    for loop review

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// print 0 to 20
+
+for (i=0; i<=20; i++){
+   console.log(i);
+}
+
+//print 3-29 odd numbers
+
+for (i=3; i<30; i++){
+   if (i%2!=0){
+      console.log(i);
+   }
+}
+
+//print 12 to -14 even descending
+
+for (j=12; j>=-14; j--){
+   if (j%2==0){
+      console.log(j);
+   }
+}
+
+//print 50 t0 20 descending if only multiplication of 3
+
+for (k=50; k>=20; k--){
+   if (k%3==0){
+      console.log(k);
+   }
+}
